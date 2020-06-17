97e48b18d75229ad8743c7333ea32aadf010059e
alibhr <ali.bahar12389@gmail.com>
2020-06-18 00:59:42 +0300

    Revert "Merge branch 'gui/sample' into develop-statistic"
    This reverts commit 929dfc61466a2d0e941f9d0728503f886b705723, reversing
    changes made to 3fce81162594e85428ce8bdb5bdc949cf93559fb.

--- src/sve/gui/sample/environmentConstants.java
+++
@@ -1,9 +0,0 @@
-package sve.gui.sample;
-
-public interface environmentConstants {
-    Integer MAIN_WIDTH=1280;
-    Integer MAIN_HEIGHT=960;
-    Integer CANVAS_WIDTH=1280;
-    Integer CANVAS_HEIGHT=960;
-    Integer NXN=64;
-}
